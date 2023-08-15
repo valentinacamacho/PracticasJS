7002509094fabacd9ca15aecfd10309802e6c4ff
valentinacamacho <valentinacamacho0206@gmail.com>
2023-08-14 21:00:11 -0500

    exercise1 is a simple greeting with an alert and add a variable

---
+++ js/Ejercicio1.js
@@ -0,0 +1,8 @@
+/**
+ * Ejercicio 1: Saludo simple
+Crea un programa que utilice alert() para mostrar el mensaje: "¡Hola! ¡Bienvenido/a  a JavaScript!"
+ */
+
+let nombre=prompt("Ingresa tu nombre")
+
+alert(" ¡Hola! " + nombre + " Bienvenid@ a JavaScript")
